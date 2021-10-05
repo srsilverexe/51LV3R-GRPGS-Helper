9788a9f134e6a5a646457dfe0271c75b86be19d5
Sr.エドワード (Sr.51LV3R) <kratos010307@gmail.com>
2021-10-04 22:26:04 -0300

    Minor sheet manager fix

---
+++ Scripts/SheetManager.gd
@@ -0,0 +1,6 @@
+extends Control
+
+func _on_TextureButton_button_down():
+	# warning-ignore:return_value_discarded
+	get_tree().change_scene("res://Scenes/Menu.tscn")
+	pass # Replace with function body.
